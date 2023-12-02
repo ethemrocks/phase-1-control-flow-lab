a227c936cf30e212ab1bc1d5556e27a4f3205188
Ethem Kacic <ethem3k@gmail.com>
2023-12-02 14:25:23 -0500

    done

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+// Function to check scuber greeting based on distance in feet
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// Function to check city and return a response using ternary operator
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Function to switch on charm based on tip value
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
+// Export functions if needed (assuming you are using CommonJS modules)
+module.exports = {
+  scuberGreetingForFeet,
+  ternaryCheckCity,
+  switchOnCharmFromTip,
+};
